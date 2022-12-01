21939a6161b0d1590d09887069ffafecdbbe57c1
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-02 00:26:48 +0100

    add model

---
+++ src/models/indicators-coefs.ts
@@ -0,0 +1,3 @@
+import { Indicators } from "./indicators.enum";
+
+export type IndicatorsCoeffs = { [key in Indicators]: number }
---
+++ src/models/indicators.enum.ts
@@ -0,0 +1,5 @@
+export enum Indicators {
+    HEALTH,
+    ENJOYMENT,
+    MONEY
+}
