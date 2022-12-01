9091d3e6010c125e68c32a929194952e4925a722
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-02 00:19:32 +0100

    create game layouts

--- src/pages/Contact.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-    return <>
-
-        <h1 className="text-3xl">Je test un truc hihi</h1>
-
-    </>
-
-}
---
+++ src/pages/Game.tsx
@@ -0,0 +1,8 @@
+export default function Game() {
+    return <>
+
+        <p>Jeu</p>
+
+    </>
+
+}
