da1360f5fbb67b3edd72e9304e0595638f79a2f9
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-02 03:41:55 +0100

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { Link, Outlet } from "react-router-dom";
 import Logo from './images/Ratisexe-logo.png'
 
 export default function App() {
-  
+
   axios.defaults.baseURL = "https://api.ratiscrum.tgimenez.fr";
 
   return <main className="flex h-full w-full flex-col overflow-hidden overscroll-none">
 
     <Link to={'/'} className="absolute top-5 left-5 btn btn-ghost">
-      <img src={Logo} width={150}></img>
+      <img src={Logo} className="w-24 md:w-34"></img>
     </Link>
 
-    <div className="justify container mx-auto py-14 flex h-full w-full flex-col items-center justify-center gap-5 align-middle">
+    <div className="justify container mx-auto py-4 md:py-8 flex h-full w-full flex-col items-center justify-center gap-5 align-middle overflow-y-auto overflow-x-hidden">
 
       <Outlet />
 
--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -6,16 +6,22 @@ import { Question } from "../models/question";
 import { Side } from "../models/side.enum";
 
 export function QuestionPage({ question, onChoice }: { question: Question, onChoice: (choice: Choice) => void }) {
-    return <div className="flex flex-col justify-center items-center gap-2 h-screen">
+    return <div className="flex flex-col px-4 pb-1 gap-10 w-full h-full lg:my-10 mx-5 lg:mx-0 overflow-y-auto">
 
-        {/* TODO : Implement days */}
-        <DayLabel day={1}></DayLabel>
-
-        <div className="flex flex-1 gap-2 items-center justify-center">
-            <QuestionChoice choice={question.choices[0]} side={Side.LEFT} click={() => onChoice(question.choices[0])}></QuestionChoice>
-            <QuestionCard question={question}></QuestionCard>
-            <QuestionChoice choice={question.choices[1]} side={Side.RIGHT} click={() => onChoice(question.choices[1])}></QuestionChoice>
+        <div className="grow flex justify-center items-center">
+            {/* TODO : Implement days */}
+            <DayLabel day={1}></DayLabel>
         </div>
-        <p className="text-center font-bold opacity-30 mb-5">Prenez une décision pour voir ce que les autres ont répondus !</p>
+
+        <div className="flex flex-col lg:flex-row gap-2 items-center justify-center">
+            <QuestionChoice className="order-last lg:order-first" choice={question.choices[0]} side={Side.LEFT} click={() => onChoice(question.choices[0])}></QuestionChoice>
+            <QuestionCard className="order-first" question={question}></QuestionCard>
+            <QuestionChoice className="order-last" choice={question.choices[1]} side={Side.RIGHT} click={() => onChoice(question.choices[1])}></QuestionChoice>
+        </div>
+
+        <div className="grow flex content-center items-center">
+            <p className="text-center font-bold opacity-30 mb-5">Prenez une décision pour voir ce que les autres ont répondus !</p>
+        </div>
+
     </div>
 }
