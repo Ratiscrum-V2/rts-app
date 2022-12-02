4d00508c6fd0692a2c4d05120fa6d8ecc2aac5d4
Raffael Di Pietro <raffael.dpietro@pixacare.com>
2022-12-02 07:43:58 +0100

    add descriptions

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,8 @@ export default function HomePage() {
     return <div className='flex flex-col justify-center items-center h-screen gap-2'>
         <img src={Logo} width={300}></img>
 
-        <p>Description</p>
+        <p>Le jeu pour parfaire votre education sexuelle</p>
+        <p>Faites les meilleurs choix afin de continuer l'aventure le plus longtemps possible !</p>
 
         <Link to={'/play'} className="btn btn-primary">Démarrer la partie</Link>
 
