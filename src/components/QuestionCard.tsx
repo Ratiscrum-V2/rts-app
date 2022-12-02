8dda5ca8c76a210876598de6e2a5a710ed6c4483
Raffael Di Pietro <raffael.dpietro@pixacare.com>
2022-12-02 03:51:46 +0100

    feat(question-page): add swipe gesture

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -3,13 +3,13 @@ import Logo from '../images/Ratisexe-dark.png'
 
 export function QuestionCard({ question }: { question: Question }) {
 
-    return <div className="card w-96 bg-base-content drop-shadow-xl">
+    return <div className="card w-96 bg-base-content drop-shadow-xl select-none">
         <figure className="p-3">
-            <img src="https://placeimg.com/400/225/arch" className="rounded-xl" />
+            <img src="https://placeimg.com/400/225/arch" className="rounded-xl pointer-events-none" />
         </figure>
         <div className="card-body items-center text-center">
             <h3 className="card-title text-neutral my-auto mb-6">{question.question}</h3>
         </div>
-        <img src={Logo} width={80} className="self-end absolute right-4 bottom-4"></img>
+        <img src={Logo} width={80} className="self-end absolute right-4 bottom-4 pointer-events-none"></img>
     </div>
 }
--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -4,8 +4,21 @@ import { QuestionChoice } from "../components/QuestionChoice";
 import { Choice } from "../models/choice";
 import { Question } from "../models/question";
 import { Side } from "../models/side.enum";
+import TinderCard from 'react-tinder-card'
+
 
 export function QuestionPage({ question, onChoice }: { question: Question, onChoice: (choice: Choice) => void }) {
+
+    const onSwipe = (direction: any) => {
+        console.log("YOU SWIPED " + direction);
+        if(direction === 'left') {
+            onChoice(question.choices[0]);
+        }
+        if(direction === 'right') {
+            onChoice(question.choices[1]);
+        }
+    }
+
     return <div className="flex flex-col justify-center items-center gap-2 h-screen">
 
         {/* TODO : Implement days */}
@@ -13,9 +26,17 @@ export function QuestionPage({ question, onChoice }: { question: Question, onCho
 
         <div className="flex flex-1 gap-2 items-center justify-center">
             <QuestionChoice choice={question.choices[0]} side={Side.LEFT} click={() => onChoice(question.choices[0])}></QuestionChoice>
-            <QuestionCard question={question}></QuestionCard>
+            <TinderCard 
+                onSwipe={onSwipe}
+                preventSwipe={['up', 'down']}
+                swipeRequirementType="position"
+                swipeThreshold={window.innerWidth * 0.15}
+            >
+                <QuestionCard question={question}></QuestionCard>
+            </TinderCard>
             <QuestionChoice choice={question.choices[1]} side={Side.RIGHT} click={() => onChoice(question.choices[1])}></QuestionChoice>
         </div>
         <p className="text-center font-bold opacity-30 mb-5">Prenez une décision pour voir ce que les autres ont répondus !</p>
+        <p className="text-center font-bold opacity-30 mb-5">Swipez ou cliquez !</p>
     </div>
 }
