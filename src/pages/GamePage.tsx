255632f4b91c72e2332bc706aa8df6353c7f94c0
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-02 01:38:03 +0100

    implement question page layout

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,9 +1,15 @@
+import { Choice } from "../models/choice";
 import { exampleQuestion } from "../models/mocks/question.mock";
 import { QuestionPage } from "./QuestionPage";
 
 export default function GamePage() {
 
     const question = exampleQuestion;
-    return <QuestionPage question={question}></QuestionPage>
+
+    const handleChoice = (choice: Choice) => {
+        console.log("clicked on", choice);
+    }
+
+    return <QuestionPage question={question} onChoice={handleChoice}></QuestionPage>
 
 }
